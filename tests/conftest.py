345b8c8cfe3310850ab91b0406a5262fae180f4e
Christian Assing <chris@ca-net.org>
2017-08-25 19:33:06 +0200

    run httpbin in seperate background process

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
 
-import asyncio
 import collections
-
+import contextlib
 import pytest
+import socket
+import subprocess
+import time
+
+from urllib.error import URLError
+from urllib.request import urlopen
 
 
 class Url(str):
@@ -17,12 +22,40 @@ class Url(str):
 Server = collections.namedtuple('Server', ['url', 'host', 'port'])
 
 
-@pytest.fixture
-async def httpbin(unused_tcp_port):
-    httpbin_cmd = f"python -m httpbin.core --port={unused_tcp_port}"
-    # proc = await asyncio.create_subprocess_exec(*httpbin_cmd.split(), loop=event_loop)
-    proc = await asyncio.create_subprocess_exec(*httpbin_cmd.split())
+def wait_for_httpbin(url):
+    retry = 0
+    timeout = 0.1
+    # try to reach httpbin in max 5s
+    while retry < 50:
+        try:
+            with urlopen(url) as resp:
+                assert resp.getcode() == 200
+                return
+        except URLError:
+            time.sleep(timeout)
+            retry += 1
+
+    raise Exception("httpbin server not reachable!!!")
+
+
+@pytest.fixture(scope='session')
+def tcp_port():
+    """Find an unused localhost TCP port from 1024-65535 and return it."""
+    with contextlib.closing(socket.socket()) as sock:
+        sock.bind(('127.0.0.1', 0))
+        return sock.getsockname()[1]
+
+
+@pytest.yield_fixture(scope='session')
+def httpbin(tcp_port):
+    httpbin_cmd = f"python -m httpbin.core --port={tcp_port}"
+    httpbin_proc = subprocess.Popen(httpbin_cmd.split(" "))
     host = "localhost"
-    port = unused_tcp_port
-    yield Server(Url(f"http://{host}:{port}"), host, port)
-    proc.kill()
+    port = tcp_port
+    url = Url(f"http://{host}:{port}")
+    # wait until httpbin as been started
+    wait_for_httpbin(url)
+    # return and run tests
+    yield Server(url, host, port)
+    # kill httpbin
+    httpbin_proc.kill()
