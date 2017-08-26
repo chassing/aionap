01732cf558e6850ac9697385f02d37e834bf78a4
Christian Assing <chris@ca-net.org>
2017-08-26 11:07:33 +0200

    test our httpbin fixture

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -16,3 +16,17 @@ async def test_httpbin_is_running(httpbin):
     async with aiohttp.ClientSession() as session:
         async with session.get(httpbin.url / "status/418") as resp:
             assert resp.status == 418
+
+
+async def test_httpbin_version(httpbin):
+    """/anything is available for httpbin >= 0.6.0."""
+    async with aiohttp.ClientSession() as session:
+        async with session.get(httpbin.url / "anything/anything") as resp:
+            assert resp.status == 200
+
+
+async def test_httpbin_std_redirect(httpbin):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(httpbin.url / "redirect/1", allow_redirects=False) as resp:
+            assert resp.headers['location'] == '/get'
+            assert resp.status == 302
